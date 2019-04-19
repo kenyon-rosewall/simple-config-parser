b564ded49f80289a6fddb58f49cb2768eca4d13c
Kenyon R <kenyon@gyra.cc>
2019-04-19 15:57:29 -0700

    Basic line parsing with errors

--- simple_config_parser.rb
+++ simple_config_parser.rb
@@ -7,4 +7,22 @@ class SimpleConfigParser
 			raise IOError, "Config file not found"
 		end
 	end
+
+	def parse_line(line)
+		# A correctly formatted line will include an assignment operator, here =
+		# Stripping each value in the array will get rid of extra white space
+		config_arr = line.split('=').map(&:strip)
+
+		# Either too few or too many values in the line, error
+		if (config_arr.count != 2)
+			raise StandardError, "Line is not formatted correctly"
+		end
+		# If a value wasn't set on either side of the assignment operator, error
+		if (config_arr[0] == '' or config_arr[1] == '')
+			raise StandardError, "Line is not formatted correctly"
+		end
+
+		# Return the cleaned array of key,value
+		config_arr
+	end
 end
--- spec/simple_config_parser_spec.rb
+++ spec/simple_config_parser_spec.rb
@@ -19,4 +19,32 @@ describe SimpleConfigParser do
 			expect{parser = SimpleConfigParser.new(ex_config_file)}.to raise_error(IOError)
 		end
 	end
+
+	describe "#parse_line" do
+		[
+			'this is just text',
+			'foo=',
+			'=bar',
+			'foo = bar=baz'
+		].each do |line|
+			it "should error on invalid line" do
+				parser = SimpleConfigParser.new
+
+				expect{output = parser.parse_line(line)}.to raise_error(StandardError)
+			end
+		end
+
+		{
+			'foo=bar' => ['foo', 'bar'],
+			'baz = bip' => ['baz', 'bip']
+		}.each do |line, expected_output|
+			it "should return array on valid line" do
+				parser = SimpleConfigParser.new
+
+				output = parser.parse_line(line)
+
+				expect(output).to eq(expected_output)
+			end
+		end
+	end
 end
