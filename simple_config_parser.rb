fbd381110c9d0014914c2b8fcbe39439b2b008f7
Kenyon R <kenyon@gyra.cc>
2019-04-19 15:28:01 -0700

    Enable loading config files

--- simple_config_parser.rb
+++ simple_config_parser.rb
@@ -1,5 +1,10 @@
 class SimpleConfigParser
-	def initialize
-		puts 'new SimpleConfigParser created'
+	attr_accessor :config_file
+
+	def initialize(filename = './config.txt')
+		@config_file = filename
+		if !File.exists?(@config_file)
+			raise IOError, "Config file not found"
+		end
 	end
 end
--- spec/simple_config_parser_spec.rb
+++ spec/simple_config_parser_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe SimpleConfigParser do
 	describe "#new" do
-		it "should print out hello world" do
-			expect(STDOUT).to receive(:puts).with('new SimpleConfigParser created')
+		it "should load a config file" do
+			ex_config_file = './config.txt'
+			new_config_file = './config2.txt'
+
 			parser = SimpleConfigParser.new
+			parser2 = SimpleConfigParser.new(new_config_file)
+
+			expect(parser.config_file).to eq(ex_config_file)
+			expect(parser2.config_file).to eq(new_config_file)
+		end
+
+		it "should throw exception if config file doesn't exist" do
+			ex_config_file = './config3.txt'
+
+			expect{parser = SimpleConfigParser.new(ex_config_file)}.to raise_error(IOError)
 		end
 	end
 end
